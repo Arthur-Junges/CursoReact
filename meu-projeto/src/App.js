df40b8b158373eb8216ae2236f4ffa315f2496f7
Arthur-Junges <Arthur.jungesH@gmail.com>
2026-03-29 19:09:05 -0300

    CSS module

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,16 +1,19 @@
 
 import './App.css';
 //import HelloWorld from './components/HelloWorld'; //Sempre chamar com uma tag ex: <HelloWorld/>
-import SayMyName from './components/SayMyName';
-import Pessoa from './components/Pessoa';
+//import SayMyName from './components/SayMyName';
+//import Pessoa from './components/Pessoa';
 //import Aula01 from './aulas/Aula01';
 //import Aula02 from './aulas/Aula02';
+import Aula03 from './aulas/Aula03';
+//import Frase from './components/Frase';
+
 
 function App() {
 
   return (
     <div className="App">
-  
+      <Aula03/>
     </div>
   );
 }
---
+++ meu-projeto/src/aulas/Aula03.jsx
@@ -0,0 +1,13 @@
+import styles from "./Aula03.module.css";
+
+function Aula03() {
+    return (
+        <div className={styles.fraseContainer}>
+            <p className={styles.fraseContent}>
+                Este é um componente com uma frase
+            </p>
+        </div>
+    );
+}
+
+export default Aula03;
