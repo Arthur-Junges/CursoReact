469856cb08a16281fae43cbed429f0a0cb77906e
Arthur-Junges <Arthur.jungesH@gmail.com>
2026-03-31 18:10:44 -0300

    React Fragments

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,19 +1,12 @@
 
 import './App.css';
-//import HelloWorld from './components/HelloWorld'; //Sempre chamar com uma tag ex: <HelloWorld/>
-//import SayMyName from './components/SayMyName';
-//import Pessoa from './components/Pessoa';
-//import Aula01 from './aulas/Aula01';
-//import Aula02 from './aulas/Aula02';
-import Aula03 from './aulas/Aula03';
-//import Frase from './components/Frase';
-
+import Aula04 from './aulas/Aula04';
 
 function App() {
 
   return (
     <div className="App">
-      <Aula03/>
+      <Aula04/>
     </div>
   );
 }
---
+++ meu-projeto/src/aulas/Aula04.jsx
@@ -0,0 +1,12 @@
+import Lista from "../components/Lista"
+
+function Aula04() {
+    return (
+        <div>
+            <h2>Aula 04 - Listas e Props</h2>
+            <Lista />
+        </div>
+    )
+}
+
+export default Aula04
